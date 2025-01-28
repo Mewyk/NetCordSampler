63c22112d62ecc8778abeb4220a671c3331d852f
Mewyk <Mewyk@UnicornDelight.com>
2025-01-28 12:14:58 -0800

    Removed obsolete code

--- NetCordSampler.Utilities/SummaryAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace NetCordSampler.Utilities;
-
-[AttributeUsage(AttributeTargets.Field)]
-public class SummaryAttribute(string description) : Attribute
-{
-    public string Description { get; } = description;
-}
