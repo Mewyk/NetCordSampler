e9eeca4b319f2cad411063ecdda611e366803b75
Mewyk <Mewyk@UnicornDelight.com>
2025-01-28 14:33:53 -0800

    Refactored Builder to actually handle the building directly
    This leaves the `CodeSample` files to be strictly used for default values and things specific to the object it is.
    
    `Program.cs` examples and incomplete functionality that had usage of the builder was also updated to reflect these changes.

---
+++ NetCordSampler/CodeSamples/CodeSampleLocator.cs
@@ -0,0 +1,42 @@
+using System.Reflection;
+
+using NetCordSampler.Interfaces;
+
+namespace NetCordSampler.CodeSamples;
+
+public static class CodeSampleLocator
+{
+    private static readonly Dictionary<Type, object> _codeSamples = new();
+
+    static CodeSampleLocator()
+    {
+        var sampleTypes = Assembly.GetExecutingAssembly()
+            .GetTypes()
+            .Where(
+                type => !type.IsAbstract
+                && type.GetInterfaces().Any(
+                    interfaceType => interfaceType.IsGenericType
+                    && interfaceType.GetGenericTypeDefinition() == typeof(ICodeSample<>)))
+            .ToList();
+
+        foreach (var type in sampleTypes)
+        {
+            var interfaces = type.GetInterfaces().Where(
+                interfaceType => interfaceType.IsGenericType
+                && interfaceType.GetGenericTypeDefinition() == typeof(ICodeSample<>));
+
+            foreach (var interfaceItem in interfaces)
+            {
+                var targetType = interfaceItem.GetGenericArguments()[0];
+                var instance = Activator.CreateInstance(type);
+                if (instance != null)
+                    _codeSamples[targetType] = instance;
+            }
+        }
+    }
+
+    public static ICodeSample<T>? GetCodeSample<T>() where T : class => 
+        _codeSamples.TryGetValue(typeof(T), out var codeSample)
+            ? codeSample as ICodeSample<T>
+            : null;
+}
