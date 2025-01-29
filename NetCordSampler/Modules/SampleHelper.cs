07d4a3d32548e36577181cb5b4f27fc20cb7e954
Mewyk <Mewyk@UnicornDelight.com>
2025-01-28 18:05:47 -0800

    Minor update to the SampleObject class

--- NetCordSampler/Modules/SampleHelper.cs
+++ NetCordSampler/Modules/SampleHelper.cs
@@ -18,7 +18,11 @@ public static class SampleHelper
 
     public class SampleObject
     {
-        public string FullName { get; set; } = string.Empty;
+        public string FullName =>
+            string.IsNullOrEmpty(Namespace)
+            ? Name
+            : $"{Namespace}.{Name}";
+
         public string Namespace { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty; // Short description derived from xml summary
